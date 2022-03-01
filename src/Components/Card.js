07bd23fb4cb839fd0a83b57409dcbbbec8dd5b66
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2022-03-01 12:47:50 +0530

    intial setu socialReact

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css';
+import CardDetails from './CardDetails';
+
+const Card = () => {
+    return (
+        <div className='Card-main'>
+            Tata Steal
+            <CardDetails/>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/Components/CardDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Carddetails = () => {
+    return (
+        <div>
+        Total sales
+
+        </div>
+    );
+}
+
+export default Carddetails;
+ 
